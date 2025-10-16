9f6f2e81d33f446e6b52b7c5f15c6f379a8eb7b9
Manas914 <manasduttarai3@gmail.com>
2025-10-16 08:43:01 +0530

    making a student table

---
+++ Making a student table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Student (
+    StudentId NUMBER(4) PRIMARY KEY,
+    Student_name VARCHAR2(40) NOT NULL,
+    Address1 VARCHAR2(300),
+    Gender VARCHAR2(15),
+    Course VARCHAR2(8)
+);
